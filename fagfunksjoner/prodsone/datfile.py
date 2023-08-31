af8f9573d4172e57e6b55441ab73f848aea73080
Carl Corneil <cfc@ssb.no>
2023-08-31 08:50:59 +0200

    Update datfile.py

--- fagfunksjoner/prodsone/datfile.py
+++ fagfunksjoner/prodsone/datfile.py
@@ -5,24 +5,24 @@ def read_datfile(path: str, input_setn: str) -> pd.DataFrame:
     # Parse input
     spots = {}
     for row in input_setn.split("\n"):
-        row = row.replace("@", "")
-        name = row.strip().split(" ")[1]
-        start = row.strip().split(" ")[0]
-        width = "".join([c for c in row.strip().split(" ")[-1] if c.isdigit()])
-        spots[name] = (start, width)
+        if row.strip():
+            row = row.replace("@","")
+            name = row.strip().split(" ")[1]
+            start = row.strip().split(" ")[0]
+            width = "".join([c for c in row.strip().split(" ")[-1].split(".")[0] if c.isdigit()])
+            spots[name] = (start, width)
     widths = []
     names = []
     i = 1
-    # Create widths and names from parsed input
     for k, v in spots.items():
         name = k
-        k = v[0]
-        v = v[1]
+        k = int(v[0])
+        v = int(v[1])
         if i == 1 and k != 1:
-            widths.append(int(k) - 1)
+            widths.append(k-1)
             names.append(f"delete_{i}")
-        elif int(k) > i + 1:
-            widths.append(int(k) - i - 1)
+        elif int(k) > i+1:
+            widths.append(k-i-1)
             names.append(f"delete_{i}")
         widths.append(int(v))
         names.append(name)
