7e00ff9537c7df6f38142e1e4b96fb8fef2a71d5
ort <ort@ssb.no>
2023-11-22 09:04:45 +0100

    period methods

--- fagfunksjoner/paths/daplapath.py
+++ fagfunksjoner/paths/daplapath.py
@@ -1,11 +1,10 @@
-from abc import ABC, abstractproperty, abstractmethod
+from abc import ABC, abstractmethod
 from pathlib import PurePosixPath, PurePath
 import re
 import warnings
 from os import PathLike
 
 import datetime
-import duckdb
 import dapla as dp
 import geopandas as gpd
 import numpy as np
@@ -19,6 +18,10 @@ import sgis as sg
 
 VERSION_PATTERN = r"_v(\d+)"
 VERSION_PREFIX = "_v"
+
+PERIOD_PATTERN = r"_p(\d{4}(?:-\d{2}(?:-\d{2})?)?)"
+PERIOD_PREFIX = "_p"
+
 PRE_TEAMNAME = "ssb-prod-"
 GS_URI_PREFIX = "gs://"
 
@@ -34,7 +37,13 @@ def _pathseries_constructor_with_fallback(data=None, index=None, **kwargs):
 class PathSeries(pd.Series):
     _version_pattern = VERSION_PATTERN
     _version_prefix = VERSION_PREFIX
-    _metadata = ["_version_pattern", "_max_rows", "_max_colwidth", "_defined_name", "name"]
+    _metadata = [
+        "_version_pattern",
+        "_max_rows",
+        "_max_colwidth",
+        "_defined_name",
+        "name",
+    ]
 
     def __init__(
         self,
@@ -45,11 +54,11 @@ class PathSeries(pd.Series):
         **kwargs,
     ):
         super().__init__(data, index=index, **kwargs)
-        
+
         self._max_rows = max_rows
         self._max_colwidth = max_colwidth
         pd.set_option("display.max_colwidth", max_colwidth)
-        
+
     @property
     def _constructor(self):
         return _pathseries_constructor_with_fallback
@@ -64,13 +73,13 @@ class PathSeries(pd.Series):
         if len(self) <= 1:
             return Path("")
 
-        splitted_path: list[str] = PurePosixPath(self.iloc[0]).as_posix().split("/")
+        splitted_path: list[str] = self.iloc[0].split("/")
 
-        common_path = [
+        common_parts = [
             folder for folder in splitted_path if self.str.contains(folder).all()
         ]
 
-        return Path("/".join(common_path))
+        return Path("/".join(common_parts))
 
     def files(self):
         """Return only the files."""
@@ -105,12 +114,12 @@ class PathSeries(pd.Series):
 
     def within_hours(self, hours: int):
         """Select files with an 'updated' timestamp within the given number of hours."""
-        time_then = pd.Timestamp.now() - pd.Timedelta(minutes=hours * 60)
+        time_then = pd.Timestamp.now() - pd.Timedelta(hours=hours)
         return self.loc[lambda x: x.updated > time_then]
 
-    def within_days(self, hours: int):
+    def within_days(self, days: int):
         """Select files with an 'updated' timestamp within the given number of days."""
-        time_then = pd.Timestamp.now() - pd.Timedelta(minutes=hours * 60 * 24)
+        time_then = pd.Timestamp.now() - pd.Timedelta(days=days)
         return self.loc[lambda x: x.updated > time_then]
 
     @property
@@ -136,7 +145,7 @@ class PathSeries(pd.Series):
     @property
     def names(self):
         return self.apply(lambda x: x.name)
-    
+
     @property
     def version_number(self):
         return self.apply(lambda x: x.version_number)
@@ -188,7 +197,7 @@ class PathSeries(pd.Series):
         only_newest = without_version_number.loc[lambda x: ~x.duplicated(keep="last")]
 
         return self.iloc[only_newest.index]
-    
+
     def __str__(self):
         repr_params = fmt.get_series_repr_params()
         repr_params["max_rows"] = self._max_rows
@@ -207,36 +216,48 @@ class PathSeries(pd.Series):
             )
 
         return s.to_string(**repr_params)
-        
+
     def __repr__(self):
         return self.__str__()
 
     def _repr_html_(self):
-        df = pd.DataFrame({"path": self})#self + '#' + self.stem})
+        df = pd.DataFrame({"path": self})  # self + '#' + self.stem})
+        if not len(df):
+            return df._repr_html_()
+
         df.index = pd.MultiIndex.from_arrays(
-            [self.index.get_level_values(0), self.index.get_level_values(1).astype(int)],
+            [
+                self.index.get_level_values(0),
+                self.index.get_level_values(1).astype(int),
+            ],
             names=["updated", "mb (int)"],
         )
-        
+
         if len(df) <= self._max_rows:
-            return df.style.format({'path': split_path_and_make_copyable_html}).to_html()
-        
+            return df.style.format(
+                {"path": split_path_and_make_copyable_html}
+            ).to_html()
+
         # the Styler puts the elipsis row last. I want it in the middle. Doing it manually...
-        first_rows = df.head(self._max_rows // 2).style.format({'path': split_path_and_make_copyable_html})
-        last_rows = df.tail(self._max_rows // 2).style.format({'path': split_path_and_make_copyable_html})
+        first_rows = df.head(self._max_rows // 2).style.format(
+            {"path": split_path_and_make_copyable_html}
+        )
+        last_rows = df.tail(self._max_rows // 2).style.format(
+            {"path": split_path_and_make_copyable_html}
+        )
 
         elipsis_row = df.iloc[[0]]
-        elipsis_row.index = pd.MultiIndex.from_arrays([["..."], ["..."]], names=elipsis_row.index.names)
+        elipsis_row.index = pd.MultiIndex.from_arrays(
+            [["..."], ["..."]], names=elipsis_row.index.names
+        )
         elipsis_row.iloc[[0]] = ["..."] * len(elipsis_row.columns)
         elipsis_row = elipsis_row.style
-        
+
         return first_rows.concat(elipsis_row).concat(last_rows).to_html()
 
 
 def split_path_and_make_copyable_html(
-    path: str, 
-    split: str | None = "/", 
-    display_prefix: str | None = ".../"
+    path: str, split: str | None = "/", display_prefix: str | None = ".../"
 ) -> str:
     """Get html text that displays the last part, but makes the full path copyable to clipboard."""
 
@@ -267,7 +288,7 @@ def split_path_and_make_copyable_html(
     }}
     </script>
     """
-    
+
     if split is not None:
         name = path.split(split)[-1]
         displayed_text = f"{display_prefix}{name}" if display_prefix else name
@@ -275,8 +296,8 @@ def split_path_and_make_copyable_html(
         displayed_text = path
 
     return (
-        copy_to_clipboard_js + 
-        f'<a href="{displayed_text}" onclick="copyToClipboard(\'{path}\')">{displayed_text}</a>'
+        copy_to_clipboard_js
+        + f'<a href="{displayed_text}" onclick="copyToClipboard(\'{path}\')">{displayed_text}</a>'
     )
 
 
@@ -298,17 +319,39 @@ class Path(str):
 
     _version_pattern = VERSION_PATTERN
     _version_prefix = VERSION_PREFIX
+    _period_pattern = PERIOD_PATTERN
+    _period_prefix = PERIOD_PREFIX
 
     def __new__(cls, gcs_path: str | PurePath):
         gcs_path = cls._fix_path(gcs_path)
-        return str.__new__(cls, gcs_path)
-    
-    def __init__(self, gcs_path: str | PathLike):
-        self._path = PurePosixPath(self)
+        obj = super().__new__(cls, gcs_path)
+        obj._path = PurePosixPath(gcs_path)
+        return obj
+
+    def __getattribute__(self, name):
+        """stackoverflow hack to ensure we return Path in all but the str magigmethods (chiefly __add__)."""
+        # not a string method
+        if name not in dir(str):
+            return super().__getattribute__(name)
+
+        def method(self, *args, **kwargs):
+            value = getattr(super(), name)(*args, **kwargs)
+            # not every string method returns a str:
+            if isinstance(value, str):
+                return type(self)(value)
+            elif isinstance(value, list):
+                return [type(self)(i) for i in value]
+            elif isinstance(value, tuple):
+                return tuple(type(self)(i) for i in value)
+            else:  # dict, bool, or int
+                return value
+
+        return method.__get__(self)  # bound method
 
     @staticmethod
     def _fix_path(path: str | PurePosixPath) -> str:
-        return (str(path)
+        return (
+            str(path)
             .replace("\\", "/")
             .replace(r"\"", "/")
             .replace("//", "/")
@@ -320,11 +363,27 @@ class Path(str):
         files_in_folder = self.parent.files()
         return files_in_folder.loc[lambda x: x.str.contains(self.versionless_stem)]
 
-    def new_version(self):
+    def new_version(self, timeout: int | None = 30):
         """Return the path with the highest existing version number plus 1."""
-        versions: PathSeries = self.versions()
+        try:
+            highest_numbered: Path = self.highest_numbered_version()
+        except FileNotFoundError:
+            return self.with_version(1)
 
-        highest_numbered: Path = self.highest_numbered_version()
+        if not timeout:
+            return highest_numbered.add_to_version_number(1)
+
+        updated: PathSeries = highest_numbered.ls().updated
+        assert len(updated) == 1
+
+        time_should_be_at_least = pd.Timestamp.now() - pd.Timedelta(minutes=timeout)
+        if updated[0] > time_should_be_at_least:
+            raise ValueError(
+                f"Latest version of the file was updated {updated[0]}, which "
+                f"is less than the timeout period of {timeout} minutes. "
+                "Change the timeout argument, but be sure to not save new "
+                "versions in a loop."
+            )
 
         return highest_numbered.add_to_version_number(1)
 
@@ -336,8 +395,15 @@ class Path(str):
         """
         versions: PathSeries = self.versions()
 
-        last = versions.keep_latest_versions()
-        assert len(last) == 1
+        if not len(versions):
+            raise FileNotFoundError(self)
+
+        last = versions.keep_latest_versions(include_versionless=include_versionless)
+        if len(last) > 1:
+            raise ValueError(
+                "More than one file in the directory matches "
+                f"the versionless pattern. [{', '.join(list(last))}]"
+            )
         return last.iloc[0]
 
     def highest_numbered_version(self):
@@ -348,8 +414,15 @@ class Path(str):
         """
         versions: PathSeries = self.versions()
 
+        if not len(versions):
+            raise FileNotFoundError(self)
+
         last = versions.keep_highest_numbered_versions()
-        assert len(last) == 1
+        if len(last) > 1:
+            raise ValueError(
+                "More than one file in the directory matches "
+                f"the versionless pattern. [{', '.join(list(last))}]"
+            )
         return last.iloc[0]
 
     @property
@@ -359,6 +432,17 @@ class Path(str):
             return int(last_match)
         except IndexError:
             return None
+
+    @property
+    def periods(self) -> list[str]:
+        try:
+            return re.findall(self._period_pattern, self)
+        except IndexError:
+            return []
+
+    @property
+    def periodless_stem(self):
+        return Path(re.sub(f"{self._period_pattern}.*", "", self._path.stem))
 
     @property
     def versionless_stem(self):
@@ -415,7 +499,13 @@ class Path(str):
         with self.open() as f:
             meta = pyarrow.parquet.read_metadata(f)
             return meta.num_rows, meta.num_columns
-    
+
+    def open(self):
+        return dp.FileClient.get_gcs_file_system().open(self)
+
+    def exists(self) -> bool:
+        return dp.FileClient.get_gcs_file_system().exists(self)
+
     def mv(self, new_path: str):
         dp.FileClient.get_gcs_file_system().mv(self, as_str(new_path))
         return Path(new_path)
@@ -433,17 +523,13 @@ class Path(str):
     def is_file(self) -> bool:
         return not dp.FileClient.get_gcs_file_system().isdir(self)
 
-    def exists(self) -> bool:
-        return dp.FileClient.get_gcs_file_system().exists(self)
-
-    def open(self):
-        return dp.FileClient.get_gcs_file_system().open(self)
-
     def dirs(self, recursive: bool = False) -> PathSeries:
         """Lists all child directories."""
         if not recursive:
             info: list[dict] = dp.FileClient.get_gcs_file_system().ls(self, detail=True)
-            return PathSeries(self._get_directory_series(info).apply(Path).sort_index(level=0))
+            return PathSeries(
+                self._get_directory_series(info).apply(Path).sort_index(level=0)
+            )
 
         return self.ls(recursive=recursive).dirs()
 
@@ -451,7 +537,9 @@ class Path(str):
         """Lists all files in the directory."""
         if not recursive:
             info: list[dict] = dp.FileClient.get_gcs_file_system().ls(self, detail=True)
-            return PathSeries(self._get_file_series(info).apply(Path).sort_index(level=0))
+            return PathSeries(
+                self._get_file_series(info).apply(Path).sort_index(level=0)
+            )
 
         return self.ls(recursive=recursive).files()
 
@@ -478,6 +566,30 @@ class Path(str):
             return out.sort_index(level=0)
 
         return pd.concat([out] + more_files).sort_index(level=0)
+
+    def with_suffix(self, suffix: str):
+        return Path(f"{self.parent}/{self.stem}{suffix}")
+
+    def with_version(self, version: int):
+        return Path(
+            f"{self.parent}/{self.versionless_stem}{self._version_prefix}{version}{self.suffix}"
+        )
+
+    def with_periods(self, *p: str):
+        periods = "".join([self._period_prefix + str(x) for x in p])
+        version = (
+            ""
+            if not self.version_number
+            else f"{self._version_prefix}{self.version_number}"
+        )
+        return Path(
+            f"{self.parent}/{self.periodless_stem}{periods}{version}{self.suffix}"
+        )
+
+    def add_to_version_number(self, number: int):
+        """Add a number to the version number."""
+        new_version = self.version_number + number
+        return self.with_version(new_version)
 
     def read_pandas(
         self,
@@ -535,10 +647,13 @@ class Path(str):
             timeout: Minutes that must pass between each new version is written.
                 To avoid accidental loop writes.
         """
-        latest = self.latest_version()
+        try:
+            highest_numbered: Path = self.highest_numbered_version()
+        except FileNotFoundError:
+            return self.with_version(1)
 
         if timeout:
-            updated = latest.ls().updated
+            updated = highest_numbered.ls().updated
             assert len(updated) == 1
             time_should_be_at_least = pd.Timestamp.now() - pd.Timedelta(minutes=timeout)
             if updated[0] > time_should_be_at_least:
@@ -551,28 +666,20 @@ class Path(str):
 
         if check_if_equal:
             if isinstance(df, gpd.GeoDataFrame):
-                latest_df = latest.read_geopandas()
+                highest_numbered_df = highest_numbered.read_geopandas()
             else:
-                latest_df = latest.read_pandas()
-            if latest_df.equals(df):
+                highest_numbered_df = highest_numbered.read_pandas()
+            if highest_numbered_df.equals(df):
                 return
 
         f = sg.write_geopandas if isinstance(df, gpd.GeoDataFrame) else dp.write_pandas
-        return f(df, latest.add_to_version_number(1))
+        return f(df, highest_numbered.add_to_version_number(1))
 
     def write_versionless(self, df: pd.DataFrame) -> None:
         """Write a DataFrame to the versionless path."""
         f = sg.write_geopandas if isinstance(df, gpd.GeoDataFrame) else dp.write_pandas
 
         return f(df, self.versionless)
-
-    def with_suffix(self, suffix: str):
-        return Path(f"{self.parent}/{self.stem}{suffix}")
-
-    def with_version(self, version: int):
-        return Path(
-            f"{self.parent}/{self.versionless_stem}{self._version_prefix}{version}{self.suffix}"
-        )
 
     def _get_directory_series(self, info):
         dirs = np.array([x["name"] for x in info if x["storageClass"] == "DIRECTORY"])
@@ -612,11 +719,6 @@ class Path(str):
             .loc[lambda x: ~x.str.endswith("/")]
             .sort_index(level=0)
         )
-  
-    def add_to_version_number(self, number: int):
-        """Add a number to the version number."""
-        new_version = self.version_number + number
-        return self.with_version(new_version)
 
     def __truediv__(self, other: str | PathLike | PurePath):
         """Append a string or Path to the path with a forward slash."""
@@ -643,4 +745,3 @@ def get_files_in_subfolders(folderinfo: PathSeries) -> list[PathSeries]:
         folderinfo = pd.concat(new_folderinfo)
 
     return fileinfo
-
