0f401eb10d32857167d0233e8fe96340e61a3fa4
Jonathan Husø <91268646+Jonathan7SSB@users.noreply.github.com>
2023-12-07 15:45:39 +0100

    new adj test

--- tests/dapla/test_time.py
+++ tests/dapla/test_time.py
@@ -6,77 +6,77 @@ from fagfunksjoner.dapla.standards.time import date_time, date, month, year, wee
                          [(dt.datetime(2016, 1, 1, 12, 1, 1), "2016-01-01T12:01:01"),
                           (dt.datetime(2022, 6, 1, 12, 1, 1), "2022-06-01T12:01:01")])
 def test_date_time(date_input, exprected_string):
-    date = date_time(date_input)
-    assert date == exprected_string
+    dato = date_time(date_input)
+    assert dato == exprected_string
 
 
 @pytest.mark.parametrize("date_input,exprected_string",
                          [(dt.date(2016, 1, 1), "2016-01-01"),
                           (dt.date(2022, 6, 1), "2022-06-01")])
 def test_date(date_input, exprected_string):
-    date = date(date_input)
-    assert date == exprected_string
+    dato = date(date_input)
+    assert dato == exprected_string
 
 
 @pytest.mark.parametrize("date_input,exprected_string",
                          [(dt.date(2016, 1, 1), "2016-01"),
                           (dt.date(2022, 6, 1), "2022-06")])
 def test_month(date_input, exprected_string):
-    date = month(date_input)
-    assert date == exprected_string
+    dato = month(date_input)
+    assert dato == exprected_string
 
 
 @pytest.mark.parametrize("date_input,exprected_string",
                          [(dt.date(2016, 1, 1), "2016"),
                           (dt.date(2022, 6, 1), "2022")])
 def test_year(date_input, exprected_string):
-    date = year(date_input)
-    assert date == exprected_string
+    dato = year(date_input)
+    assert dato == exprected_string
 
 
 @pytest.mark.parametrize("date_input,exprected_string",
-                         [(dt.date(2016, 1, 1), "2015-53"),
-                          (dt.date(2022, 6, 1), "2022-22")])
+                         [(dt.date(2016, 1, 1), "2015W53"),
+                          (dt.date(2022, 6, 1), "2022W22")])
 def test_week(date_input, exprected_string):
-    date = week(date_input)
-    assert date == exprected_string
+    dato = week(date_input)
+    assert dato == exprected_string
 
 
 @pytest.mark.parametrize("date_input,exprected_string",
                          [(dt.date(2016, 1, 1), "2016-001"),
                           (dt.date(2022, 6, 1), "2022-152")])
 def test_year_days(date_input, exprected_string):
-    date = year_days(date_input)
-    assert date == exprected_string
+    dato = year_days(date_input)
+    assert dato == exprected_string
 
 
 @pytest.mark.parametrize("date_input,exprected_string",
                          [(dt.date(2016, 1, 1), "2016-Q1"),
                           (dt.date(2022, 6, 1), "2022-Q2")])
 def test_quarterly(date_input, exprected_string):
-    date = quarterly(date_input)
-    assert date == exprected_string
+    dato = quarterly(date_input)
+    assert dato == exprected_string
 
 
 @pytest.mark.parametrize("date_input,exprected_string",
                          [(dt.date(2016, 1, 1), "2016-B1"),
                           (dt.date(2022, 6, 1), "2022-B3")])
 def test_bimester(date_input, exprected_string):
-    date = bimester(date_input)
-    assert date == exprected_string
+    dato = bimester(date_input)
+    assert dato == exprected_string
 
 
 @pytest.mark.parametrize("date_input,exprected_string",
                          [(dt.date(2016, 1, 1), "2016-T1"),
                           (dt.date(2022, 6, 1), "2022-T2")])
 def test_triannual(date_input, exprected_string):
-    date = triannual(date_input)
-    assert date == exprected_string
+    dato = triannual(date_input)
+    assert dato == exprected_string
 
 
 @pytest.mark.parametrize("date_input,exprected_string",
                          [(dt.date(2016, 1, 1), "2016-H1"),
                           (dt.date(2022, 6, 1), "2022-H1")])
 def test_halfyear(date_input, exprected_string):
-    date = halfyear(date_input)
-    assert date == exprected_string
+    dato = halfyear(date_input)
+    assert dato == exprected_string
