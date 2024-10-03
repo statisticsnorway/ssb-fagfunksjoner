99c805447510ab732bc4df2f4947bd158dd9d228
ssb-jhu <91268646+Jonathan7SSB@users.noreply.github.com>
2024-10-03 13:51:08 +0000

    todays code test

--- tests/paths/test_get_latest_gcs_files.py
+++ tests/paths/test_get_latest_gcs_files.py
@@ -1,15 +1,46 @@
-from unittest.mock import Mock, patch
+from fnmatch import fnmatch
 
-from fsspec.implementations.local import LocalFileSystem
+from dapla import FileClient
 
 from fagfunksjoner.paths.versions import get_latest_gcs_files
+from .conftest import testdata_get_latest_gcs_files
 
 
-@patch("fagfunksjoner.paths.versions.FileClient")
-def test_get_latest_gcs_files(file_client_mock: Mock, testdata_get_latest_gcs_files):
-    file_client_mock.get_gcs_file_system.return_value = LocalFileSystem()
-    file_client_mock.glob.return_value = testdata_get_latest_gcs_files
+class MockFS:
 
-    latest_files = get_latest_gcs_files("ssb-test-dapla-team-data-produkt/testdata")
+    @staticmethod
+    def glob(globstr: str, detail: bool = False) -> list | dict:
+        data = testdata_get_latest_gcs_files()
+
+        files = []
+
+        for file in data.keys():
+            if fnmatch(file, globstr):
+                files.append(file)
+        
+        if detail:
+            relevant_files = {}
+            for file in files:
+                relevant_files[file] = data.get(file)
+        
+        else:
+            relevant_files = [f for f in files]
+        
+        return relevant_files
+
+
+def test_get_latest_gcs_files(monkeypatch, testdata_get_latest_gcs_files):
+
+    def mock_get_fs(*args, **kwargs):
+        return MockFS()
+
+    # apply the monkeypatch for requests.get to mock_get
+    monkeypatch.setattr(FileClient, "get_gcs_file_system", mock_get_fs)
+
+    latest_files = get_latest_gcs_files(
+        "ssb-test-dapla-team-data-produkt/testdata",
+        file_format="parquet",
+        by='version'
+        )
     print(latest_files)
     assert len(latest_files) == 2
