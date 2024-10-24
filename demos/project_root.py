a1c074476c8936333b0dc9f2423b482cda8235c1
aecorn <cfc@ssb.no>
2024-10-24 06:09:44 +0000

    testing

---
+++ demos/project_root.py
@@ -0,0 +1,26 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: py:percent
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.16.4
+#   kernelspec:
+#     display_name: ssb-fagfunksjoner
+#     language: python
+#     name: ssb-fagfunksjoner
+# ---
+
+# %%
+from fagfunksjoner import ProjectRoot
+
+# %%
+proj_root = ProjectRoot()
+
+# %%
+proj_root.path
+
+# %%
+path_equals = "PosixPath('/home/onyxia/work/ssb-fagfunksjoner')"
