5b758f1dc36577aaaae4ef1fe902b777202c8b8d
Jan Sebastian Rothe <jsr@ssb.no>
2024-10-25 22:53:46 +0200

    fixed Optional output type hint

--- src/fagfunksjoner/paths/versions.py
+++ src/fagfunksjoner/paths/versions.py
@@ -5,6 +5,7 @@ The main purpose is fileversions according to Statistics Norway standards.
 """
 
 import glob
+from typing import Optional
 
 from dapla import FileClient
 
@@ -157,7 +158,7 @@ def construct_file_pattern(filepath: str, version_denoter: str = "*") -> str:
     return f"{filepath_no_version}_v{version_denoter}{file_ext}"
 
 
-def get_fileversions(filepath: str) -> list[str] | None:
+def get_fileversions(filepath: str) -> Optional(list[str]):
     """Retrieves a list of file versions matching a specified pattern.
 
     This function generates a glob pattern based on the provided file path and retrieves
