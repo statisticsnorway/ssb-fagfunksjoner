0c8be611332e8c4b2d63bf282584b326eb1ab9f7
ssb-jhu <91268646+Jonathan7SSB@users.noreply.github.com>
2025-02-11 18:44:52 +0000

    typo

--- tests/kostra/test_kostra_kommunekorr.py
+++ tests/kostra/test_kostra_kommunekorr.py
@@ -1,31 +1,20 @@
-import pandas as pd
-
 from fagfunksjoner.kostra import kostra_kommunekorr
+import pandas as pd
 
 
 def test_kostra_kommunekorr(mock_klass_classification, mock_klass_correspondence):
-    year = "2023"
+    year = 2023
     result = kostra_kommunekorr(year)
     assert isinstance(result, pd.DataFrame)
     assert result.shape[0] == 7
-
-    exp_cols = [
-        "komnr",
-        "komnavn",
-        "fylknr",
-        "fylknavn",
-        "fylknr_eka",
-        "fylknr_eka_m_tekst",
-        "fylk_validFrom",
-        "fylk_validTo",
-        "kostragr",
-        "kostragrnavn",
-        "kostra_validFrom",
-        "kostra_validTo",
-        "landet",
-        "landet_u_oslo",
-    ]
+    
+    exp_cols = ['komnr', 'komnavn', 'fylknr', 'fylknavn', 'fylknr_eka',
+       'fylknr_eka_m_tekst', 'fylk_validFrom', 'fylk_validTo', 'kostragr',
+       'kostragrnavn', 'kostra_validFrom', 'kostra_validTo', 'landet',
+       'landet_u_oslo']
     assert result.shape[1] == len(exp_cols)
 
-    for c1, c2 in zip(exp_cols, result.columns, strict=False):
+    for c1, c2 in zip(exp_cols, result.columns):
         assert c1 == c2
+
+    
