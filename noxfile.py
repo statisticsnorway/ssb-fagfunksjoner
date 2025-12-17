12745ca89e2835ed8bd5890e8c1696939c8b1653
Arne Sørli <81353974+arneso-ssb@users.noreply.github.com>
2025-12-17 12:54:41 +0100

    Fix missing dependencies in noxfile.py

--- noxfile.py
+++ noxfile.py
@@ -166,7 +166,7 @@ def mypy(session: Session) -> None:
 def tests(session: Session) -> None:
     """Run the test suite."""
     session.install(".")
-    session.install("coverage[toml]", "pytest", "pygments", "responses")
+    session.install("coverage[toml]", "pytest", "pygments", "responses", "pytest-mock")
     try:
         session.run(
             "coverage",
@@ -200,7 +200,7 @@ def coverage(session: Session) -> None:
 def typeguard(session: Session) -> None:
     """Runtime type checking using Typeguard."""
     session.install(".")
-    session.install("pytest", "typeguard", "pygments", "responses")
+    session.install("pytest", "typeguard", "pygments", "responses", "pytest-mock")
     session.run("pytest", f"--typeguard-packages={package}", *session.posargs)
 
 
