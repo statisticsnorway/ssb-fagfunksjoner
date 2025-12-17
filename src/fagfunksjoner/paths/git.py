16d9ff0215ea8e028a9bf79de7a4388df35bcdd0
Arne Sørli <81353974+arneso-ssb@users.noreply.github.com>
2025-12-17 10:56:04 +0100

    Add function repo_root_dir from dapla-toolbelt

--- src/fagfunksjoner/paths/git.py
+++ src/fagfunksjoner/paths/git.py
@@ -1,6 +1,7 @@
 """Code that uses things from git-files."""
 
 import os
+from pathlib import Path
 
 
 def name_from_gitconfig() -> str:
@@ -28,3 +29,39 @@ def name_from_gitconfig() -> str:
             name = " ".join(line.split(" ")[2:])
     os.chdir(curr_dir)
     return name
+
+
+def repo_root_dir(directory: Path | str | None = None) -> Path:
+    """Find the root directory of a git repo, searching upwards from a given path.
+
+    Args:
+        directory: The path to search from, defaults to the current working directory.
+            The directory can be of type string or of type pathlib.Path.
+
+    Returns:
+        Path to the git repo's root directory.
+
+    Raises:
+        RuntimeError: If no .git directory is found when searching upwards.
+
+    Example:
+    --------
+    >>> import dapla as dp
+    >>> import tomli
+    >>>
+    >>> config_file = dp.repo_root_dir() / "pyproject.toml"
+    >>> with open(config_file, mode="rb") as fp:
+    >>>     config = tomli.load(fp)
+    """
+    if directory is None:
+        directory = Path.cwd()
+
+    if isinstance(directory, str):
+        directory = Path(directory)
+
+    while directory / ".git" not in directory.iterdir():
+        if directory == Path("/"):
+            raise RuntimeError(f"The directory {directory} is not in a git repo.")
+        else:
+            directory = directory.parent
+    return directory
